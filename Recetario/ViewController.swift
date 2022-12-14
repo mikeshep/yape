f688d2ca0cdf6927164161966651eceb7322c84b
Miguel Angel <mike@ios.com>
2022-12-14 12:46:55 -0600

    Initial Commit

---
+++ Recetario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recetario
+//
+//  Created by Miguel Angel Olmedo Perez on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
